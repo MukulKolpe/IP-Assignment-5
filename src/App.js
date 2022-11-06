8ad367ca4924ee429b2997806063915e9be2f603
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-11-06 13:41:35 +0530

    Added responsive navbar & footer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import Experience from "./pages/Experience/Experience";
 import Achievements from "./pages/Achievements/Achievements";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/achievements" element={<Achievements />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        🛠️ by{" "}
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://github.com/MukulKolpe"
+        >
+          @MukulKolpe
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
