ec51baf866cacc5cf0c0a67c795962227327fc70
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-11-06 12:40:45 +0530

    created initial routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+import Experience from "./pages/Experience/Experience";
+import Achievements from "./pages/Achievements/Achievements";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/achievements" element={<Achievements />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
